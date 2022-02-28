58338cb813b1e80fc19f170adcd73e5a2858ed17
binge8 <53508344+binge8@users.noreply.github.com>
2022-02-28 16:57:24 +0800

    Update pushbot.sh

--- bin/pushbot.sh
+++ bin/pushbot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "$compile" = "success" ] || [ "$organize" = "success" ] || [ "$PACKAGED_STATUS" = "success" ];then
-  curl -H "Content-Type:application/json" -X POST -d '{"token":"${{ env.pushbot }}","title":"Actions云编译-${{ env.FILE_NAME }}成功","content":"${{ env.FILE_NAME }}发布成功","template":"json"}' http://www.pushplus.plus/send
+  curl -H "Content-Type:application/json" -X POST -d '{"token":"$pushbot","title":"Actions云编译-$FILE_NAME成功","content":"$FILE_NAME发布成功","template":"json"}' http://www.pushplus.plus/send
 else
-  curl -H "Content-Type:application/json" -X POST -d '{"token":"${{ env.pushbot }}","title":"Actions云编译-${{ env.FILE_NAME }}失败","content":"${{ env.FILE_NAME }}发布失败","template":"json"}' http://www.pushplus.plus/send
+  curl -H "Content-Type:application/json" -X POST -d '{"token":"$pushbot","title":"Actions云编译-$FILE_NAME失败","content":"$FILE_NAME发布失败","template":"json"}' http://www.pushplus.plus/send
 fi
