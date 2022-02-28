12e7bd4566ed480311cf4f0f62ec039d4e5cb190
binge8 <53508344+binge8@users.noreply.github.com>
2022-02-28 16:38:06 +0800

    测试

--- bin/pushbot.sh
+++ bin/pushbot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ "${{ env.compile }}" = "success" ]] || [[ "${{ env.organize }}" = "success" ]] || [[ "${{ env.PACKAGED_STATUS }}" = "success" ]];then
+if [ "$compile" = "success" ] || [ "$organize" = "success" ] || [ "$PACKAGED_STATUS" = "success" ];then
   curl -H "Content-Type:application/json" -X POST -d '{"token":"${{ env.pushbot }}","title":"Actions云编译-${{ env.FILE_NAME }}成功","content":"${{ env.FILE_NAME }}发布成功","template":"json"}' http://www.pushplus.plus/send
 else
   curl -H "Content-Type:application/json" -X POST -d '{"token":"${{ env.pushbot }}","title":"Actions云编译-${{ env.FILE_NAME }}失败","content":"${{ env.FILE_NAME }}发布失败","template":"json"}' http://www.pushplus.plus/send
