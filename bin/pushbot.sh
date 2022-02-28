6c2357a3f7cca4378a8c881a7179a1761e64d858
“binge8” <ooxxbbbbb@gmail.com>
2022-03-01 00:46:18 +0800

    回退

--- bin/pushbot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$compile" = "success" ] || [ "$organize" = "success" ] || [ "$PACKAGED_STATUS" = "success" ];then
-  curl -H "Content-Type:application/json" -X POST -d '{"token":"$pushbot","title":"Actions云编译-$FILE_NAME成功","content":"$FILE_NAME发布成功","template":"json"}' http://www.pushplus.plus/send
-else
-  curl -H "Content-Type:application/json" -X POST -d '{"token":"$pushbot","title":"Actions云编译-$FILE_NAME失败","content":"$FILE_NAME发布失败","template":"json"}' http://www.pushplus.plus/send
-fi
